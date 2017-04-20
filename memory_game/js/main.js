637061c6b192ba05bcf789a8b3a60636b9a62582
Matthew Gray <matthewgray@Matthews-MacBook-Pro.local>
2017-04-20 15:59:09 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -20,22 +20,60 @@ var cards = [
 	cardImage: "images/king-of-diamonds.png"
 },
 ];
+var wins = 0;
+var losses = 0;
 var cardsInPlay = [];
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match");
+		//alert("You found a match");
+		var matchResult = document.createElement('h3');
+		matchResult.textContent = 'You Won';
+		document.getElementById('game-board').appendChild(matchResult);
 	} else {
-		alert("Sorry, try again.");
+		//alert("Sorry, try again.");
+		var matchResult = document.createElement('h3');
+		matchResult.textContent = 'You Lost';
+		document.getElementById('game-board').appendChild(matchResult);
 	}
 }
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
-	if (cardsInPlay.length === 2) {
+	this.setAttribute('src', cards[cardId].cardImage)
+	if (cardsInPlay.length % 2 === 0) {
 		checkForMatch();
 	}
 }
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i += 1) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		document.getElementById('game-board').appendChild(cardElement);
+		cardElement.addEventListener('click', flipCard);
+	}
+}
+var resetBoard = function () {
+	var resetCardImg = document.getElementById("game-board");
+	while (resetCardImg.hasChildNodes()) {   
+    	resetCardImg.removeChild(resetCardImg.firstChild);
+    }
+    cardsInPlay = [];
+	createBoard();
+}
+var resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', resetBoard);
+createBoard();
+
+
+
+
+
+
+
+
+
+
